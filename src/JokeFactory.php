7030f14a7aaae0d145a8d78ae8627d22f2da18e9
Gopibabu Srungavarapu <gsrungav@uncc.edu>
2020-10-13 12:27:48 -0500

    📦 NEW: initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Gopibabu\Jokes;
+
+/**
+ * Class JokeFactory
+ * @package Gopibabu\Jokes
+ */
+class JokeFactory
+{
+    /**
+     * @return string
+     */
+    public function generateJoke() {
+        return 'This is a random joke';
+    }
+
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Gopibabu\Jokes\Tests;
+
+use Gopibabu\Jokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function returnsRandomJoke()
+    {
+        $joke = new JokeFactory();
+        $this->assertSame('This is a random joke', $joke->generateJoke());
+    }
+}
